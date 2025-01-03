4f0513af9f8cff9ffc228f84df8019c6b52eb89b
Jon Mackey <jon@jonmackey.party>
2025-01-03 12:31:23 -0500

    clean up

--- updateDbWithImage.py
+++ updateDbWithImage.py
@@ -2,18 +2,16 @@ import os
 import re
 import click
 import vertexai
-import asyncio
-from cv2 import cv2
 from tinydb import TinyDB
 from dotenv import load_dotenv
-from image_creator.stable_diffusion_openvino.makeImage import makeImage, reduceTool
-from PIL import Image, ImageEnhance, ImageFilter
+from PIL import Image
 from vertexai.preview.vision_models import ImageGenerationModel
-from utils.parseUtils import create_dict, _parse_line, check_dup
+from image_creator.stable_diffusion_openvino.makeImage import reduceTool
+from utils.parseUtils import create_dict, check_dup
 
 
 def create_vertex_image(prompt, output_file):
-    load_dotenv()
+    """Sends prompt to Vertex AI to generate an image, saves to write path"""
     vertexai.init()
     model = ImageGenerationModel.from_pretrained("imagegeneration@002")
     print(f"Generating image for: {prompt}")
@@ -22,13 +20,14 @@ def create_vertex_image(prompt, output_file):
         prompt=prompt,
         number_of_images=1,
         language="en",
-        aspect_ratio="1:1",   
+        aspect_ratio="1:1"
     )
 
     images[0].save(location=output_file, include_generation_parameters=False)
 
 
-def addItemsToDb():
+def add_items_to_db():
+    """Updates Active Db with new items from txt file"""
     load_dotenv()
     filepath = os.getenv('TXTFILEPATH')
     db = TinyDB(os.getenv('DBFILEPATH'))
@@ -46,9 +45,9 @@ def addItemsToDb():
                 reduceTool(write_path)
                 im = Image.open(write_path)
                 im.show()
-                choice = click.confirm('Do you want to keep the displayed image?', default =True) 
+                choice = click.confirm('Do you want to keep the displayed image?', default =True)
             print(f"SUCCESS: SAVED {key} to: {write_path}.")
             db.insert({'title': key, 'body': value})
-                  
+
 if __name__=="__main__":
-    addItemsToDb()
+    add_items_to_db()
