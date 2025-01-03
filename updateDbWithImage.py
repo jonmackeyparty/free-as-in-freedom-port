32c3554d3227682e3c870b94e7548e6de1f4c38a
Jon Mackey <jon@jonmackey.party>
2025-01-03 12:23:20 -0500

    add support for vertex image gen

--- updateDbWithImage.py
+++ updateDbWithImage.py
@@ -1,11 +1,32 @@
 import os
 import re
 import click
+import vertexai
+import asyncio
+from cv2 import cv2
 from tinydb import TinyDB
 from dotenv import load_dotenv
-from utils.parseUtils import create_dict, _parse_line, check_dup
 from image_creator.stable_diffusion_openvino.makeImage import makeImage, reduceTool
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageFilter
+from vertexai.preview.vision_models import ImageGenerationModel
+from utils.parseUtils import create_dict, _parse_line, check_dup
+
+
+def create_vertex_image(prompt, output_file):
+    load_dotenv()
+    vertexai.init()
+    model = ImageGenerationModel.from_pretrained("imagegeneration@002")
+    print(f"Generating image for: {prompt}")
+
+    images = model.generate_images(
+        prompt=prompt,
+        number_of_images=1,
+        language="en",
+        aspect_ratio="1:1",   
+    )
+
+    images[0].save(location=output_file, include_generation_parameters=False)
+
 
 def addItemsToDb():
     load_dotenv()
@@ -20,15 +41,14 @@ def addItemsToDb():
             output_file = re.sub('[^A-Za-z0-9]+', '', key)
             write_path = f"{img_file_path}{output_file}.png"
             choice = False
-            while choice == False:
-                makeImage(f"{key}, homemade photo, amateur photo, cellphone photograph", None, None, write_path)
+            while choice is False:
+                create_vertex_image(f"{key}, cellphone photograph", write_path)
                 reduceTool(write_path)
                 im = Image.open(write_path)
                 im.show()
-                choice = click.confirm('Do you want to keep the displayed image?', default =True)
-            else:
-                print(f"SUCCESS: SAVED {key} to: {write_path}.")
-                db.insert({'title': key, 'body': value})
+                choice = click.confirm('Do you want to keep the displayed image?', default =True) 
+            print(f"SUCCESS: SAVED {key} to: {write_path}.")
+            db.insert({'title': key, 'body': value})
                   
 if __name__=="__main__":
     addItemsToDb()
